8857ff5566cc79135a33eddc814820ce9b5339d0
deepalig11 <deepalig11198@gmail.com>
2020-09-29 00:08:55 +0530

    Updates

---
+++ precache-manifest.b132350260691c70c2bf3e68d85737ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46420dbfb9b7d7f5e326cf0057905b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d2258271030d48a608d",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0a834900ab35399aa938",
+    "url": "/static/css/main.d46f70cc.chunk.css"
+  },
+  {
+    "revision": "4d2258271030d48a608d",
+    "url": "/static/js/2.14c31d71.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.14c31d71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a834900ab35399aa938",
+    "url": "/static/js/main.83913e63.chunk.js"
+  },
+  {
+    "revision": "3d1995dc61d5c5648c08",
+    "url": "/static/js/runtime-main.6d515695.js"
+  }
+]);
